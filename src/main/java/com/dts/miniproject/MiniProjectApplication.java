7756f8d67c1acfb7c1b38d29446aa5f8e7c0ed34
Kurniawan Sudirman <kurniawanplusayahibu@gmail.com>
2022-10-07 20:41:40 +0700

    first commit mini-project

---
+++ src/main/java/com/dts/miniproject/MiniProjectApplication.java
@@ -0,0 +1,14 @@
+package com.dts.miniproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniProjectApplication.class, args);
+		System.out.println("Server side is running....");
+	}
+
+}
